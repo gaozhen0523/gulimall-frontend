1c862bdbc86446315ae75c95db169f5d52cc7fd9
gaozhen0523 <gaozhen0523@gmail.com>
2024-10-02 19:28:57 -0500

    add adding functionality for menu

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,32 +1,46 @@
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    :expand-on-click-node="false"
-    show-checkbox="true"
-    node-key="catId"
-    :default-expanded-keys="expandedKey"
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button
-          v-if="node.level <= 2"
-          type="text"
-          size="mini"
-          @click="() => append(data)"
-          >Append</el-button
-        >
-        <el-button
-          v-if="node.childNodes.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-          >Delete</el-button
-        >
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox="true"
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+            >Append</el-button
+          >
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+            >Delete</el-button
+          >
+        </span>
       </span>
-    </span>
-  </el-tree>
+    </el-tree>
+
+    <el-dialog title="Tips" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="addCategory">Confirm</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -36,6 +50,8 @@ export default {
       data: [],
       menus: [],
       expandedKey: [],
+      category: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 },
+      dialogVisible: false,
       defaultProps: {
         children: "children",
         label: "name",
@@ -57,6 +73,28 @@ export default {
     },
     append(data) {
       console.log("append", data);
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+    },
+    addCategory() {
+      console.log("data", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then(({ data }) => {
+        this.$notify({
+          title: "Success",
+          message: "菜单保存成功",
+          type: "success",
+        });
+        //close dialog
+        this.dialogVisible = false;
+        this.getMenus();
+        //set default expand menu
+        this.expandedKey = [this.category.parentCid];
+      });
     },
     remove(node, data) {
       var ids = [data.catId];
